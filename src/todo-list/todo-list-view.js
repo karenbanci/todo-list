28199e6ef1876fd9574cd5d172210917a2547557
Karen <karenbanci1@gmail.com>
2022-07-16 00:21:42 -0700

    add jsx in todo-list-view

--- src/todo-list/todo-list-view.js
+++ src/todo-list/todo-list-view.js
@@ -1,22 +1,48 @@
-// import {addTask, showTasks, updateTask, removeTask} from './todo-list/todo-list';
-
+import {ToDoList} from './todo-list';
+import './todo-list.css';
 
 export default function ToDoView(){
   return (
     <div className="container">
       <div className="logo">
-        <img src="../public/images/logo.webp" alt="logo-monsters-univesity" />
+        <img src="images/logo.png" alt="logo-monsters-univesity" />
       </div>
       <div className="card-todo">
-        <img src="../public/images/prof-knight.webp" alt="prof-knight" />
-        <div className="tasks" >
-          <input type="text" className="nameTask"></input>
-          <input type="date" id="start" name="trip-start"
-       value="2018-07-22"
-       min="2018-01-01" max="2018-12-31"></input>
-          <submit type="submit" className="btn-submit-task">Adicionar</submit>
+        <img
+          className="card-image"
+          src="images/prof-knight.webp"
+          alt="prof-knight"
+        />
+
+        <div className="add-tasks">
+          <div className="taskInformation">
+            <label for="taskName">Tarefa</label>
+            <input
+              type="text"
+              placeholder="Descreva o nome da tarefa"
+              className="taskName"
+            ></input>
+          </div>
+          <div className="taskInformation">
+            <label for="taskConclution">Data de conclusão</label>
+            <input
+              type="date"
+              className="taskDate"
+              id="start"
+              name="trip-start"
+              min="2022-01-01"
+              max="2023-12-31"
+            ></input>
+          </div>
+          <input type="submit" value="Adicionar" className="btn-submit"/>
         </div>
       </div>
+        <div className="show-tasks">
+          <div className="task">
+            <h2>Name</h2>
+
+          </div>
+        </div>
     </div>
   );
 
