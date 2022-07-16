17834800c88a569d3e5c7d7b9e22fb0de364f525
Karen <karenbanci1@gmail.com>
2022-07-15 22:46:26 -0700

    ajust images

--- src/todo-list/todo-list-view.js
+++ src/todo-list/todo-list-view.js
@@ -2,10 +2,22 @@
 
 
 export default function ToDoView(){
-  return(
-    <div>
-      
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src="../public/images/logo.webp" alt="logo-monsters-univesity" />
+      </div>
+      <div className="card-todo">
+        <img src="../public/images/prof-knight.webp" alt="prof-knight" />
+        <div className="tasks" >
+          <input type="text" className="nameTask"></input>
+          <input type="date" id="start" name="trip-start"
+       value="2018-07-22"
+       min="2018-01-01" max="2018-12-31"></input>
+          <submit type="submit" className="btn-submit-task">Adicionar</submit>
+        </div>
+      </div>
     </div>
-  )
+  );
 
 }
