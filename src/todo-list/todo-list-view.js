e56608e0a84a2241c6429972604a92ce0df07fd1
Karen <karenbanci1@gmail.com>
2022-07-16 01:47:40 -0700

    add useEffect and useState for tasks

--- src/todo-list/todo-list-view.js
+++ src/todo-list/todo-list-view.js
@@ -1,7 +1,28 @@
-import {ToDoList} from './todo-list';
-import './todo-list.css';
+import React, { useState, useEffect } from "react";
+import ToDoList from "./todo-list";
+import "./todo-list.css";
 
-export default function ToDoView(){
+export default function ToDoView() {
+  // aqui estou fazendo um submite da tarefa, quando for clicar no botao de adicionar
+  const [taskName, setTaskName] = useState("");
+  const [taskDate, setTaskDate] = useState(new Date());
+  const [tasks, setTasks] = useState([]);
+
+  function handleTaskSubmit(event) {
+    event.preventDefault();
+    // setTaskName(event.targe.value);
+    console.log(taskName, taskDate);
+    ToDoList.addTask(taskName, taskDate);
+    // setTaskDate(event.targe.value);
+  }
+
+  // o useEffect vai renderizar minha tarefa dentro da div show tasks
+  useEffect(() => {
+    ToDoList.showTasks()
+      // pegando o setTasks do useState para mostrar a promessa
+      .then((tasks) => setTasks(tasks));
+  }, []);
+
   return (
     <div className="container">
       <div className="logo">
@@ -14,14 +35,16 @@ export default function ToDoView(){
           alt="prof-knight"
         />
 
-        <div className="add-tasks">
+        <form className="add-tasks" onSubmit={handleTaskSubmit}>
           <div className="taskInformation">
             <label for="taskName">Tarefa</label>
             <input
               type="text"
               placeholder="Descreva o nome da tarefa"
               className="taskName"
-            ></input>
+              value={taskName}
+              onChange={(e) => setTaskName(e.target.value)}
+            />
           </div>
           <div className="taskInformation">
             <label for="taskConclution">Data de conclusão</label>
@@ -32,18 +55,30 @@ export default function ToDoView(){
               name="trip-start"
               min="2022-01-01"
               max="2023-12-31"
+              value={taskDate}
+              onChange={(e) => setTaskDate(e.target.value)}
             ></input>
           </div>
-          <input type="submit" value="Adicionar" className="btn-submit"/>
-        </div>
+          <input
+            type="submit"
+            value="Adicionar"
+            className="btn-submit"
+            onSubmit={handleTaskSubmit}
+          />
+        </form>
       </div>
+      {/* imprimir cada tarefa que foi adicionada */}
+      {Object.keys(tasks).map((task, id) => (
         <div className="show-tasks">
-          <div className="task">
-            <h2>Name</h2>
-
+          <div className="task" key={id}>
+            <input type="checkbox" value={tasks[task].completed} />
+            <h2>
+              {tasks[task].title}
+            </h2>
+            <h3>{tasks[task].completionDate}</h3>
           </div>
         </div>
+      ))}
     </div>
   );
-
 }
