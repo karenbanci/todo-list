15cfc15de9e08951dbd801d6187bacf83d3514dd
Karen <karenbanci1@gmail.com>
2022-07-15 22:47:54 -0700

    ajust first styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import ToDoView from "./todo-list/todo-list-view";
 
 
 function App() {
-  return <div className="App">{<ToDoView/>}</div>;
+  return(
+  <div className="App">
+    <ToDoView/>
+  </div>);
 }
 
 export default App;
