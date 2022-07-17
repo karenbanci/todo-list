fd559c4ad72f87d782ebbeb3f28c79caf552110c
Karen <karenbanci1@gmail.com>
2022-07-17 16:09:00 -0700

    move firebaseconfig another path

--- src/firebase/myFirebase.js
+++ src/firebase/myFirebase.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAgW2sbgWgID1lD3IUJ0e6ZsYoIZmogwWs",
-  authDomain: "todolist-a0e80.firebaseapp.com",
-  projectId: "todolist-a0e80",
-  storageBucket: "todolist-a0e80.appspot.com",
-  messagingSenderId: "111731509566",
-  appId: "1:111731509566:web:1f0be204021055ae4e446f",
-  measurementId: "G-BY58P8ESSE",
-};
+import firebaseConfig  from "./firebaseconfig";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
